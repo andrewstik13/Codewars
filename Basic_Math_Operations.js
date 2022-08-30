88bb2307e6a61201b8de5164e7aea143ac033756
andrewstik13 <andrewstik@yandex.ru>
2022-08-30 13:58:06 +0300

    30.08

---
+++ Basic_Math_Operations.js
@@ -0,0 +1,12 @@
+function basicOp(operation, value1, value2)
+{ 
+  
+  // Code
+  switch(operation){
+      case '+': return value1 + value2;
+      case '-': return value1 - value2;
+      case '*': return value1 * value2;
+      case '/': return value1 / value2;
+      default: return 0
+     }
+}
